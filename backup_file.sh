1f16f09008a5b0d17597ed3191f45ce3e27e6c73
Angel <elakatsukinegro@gmail.com>
2022-09-14 13:33:52 -0400

    added the input and the message if file does not exist.

--- backup_file.sh
+++ backup_file.sh
@@ -4,20 +4,27 @@ function backup_file () {
 
     if [ -f $1 ]  # ?: -f stands for "file exist"
     then
-      BACK="/tmp/$(basename ${1}.$(date +%F).$$"  
+      BACK="/tmp/$(basename ${1}).$(date +%F).$$"  
           # ? basename; will take just the name of the file and not the path passed has the parameter
           # ? $(date +%F.$$  )  this will print the date in YYYY/MM/DD
           # ? the .$$ will print the current process PID ( process ID of the linux shell ) 
 
-      echo "Baccking up $1 to ${BACK} 
+      echo "Backing up $1 to ${BACK}"
       cp $1 $BACK 
+    else 
+      # The file does not exist, this will return a exit code 1. 
+      return 1  
     fi
 }
 
-backup_file /etc/hosts
+read -p "Insert here the path of the file: " fileToBackup
 
-if [ $? -eq 0 ]
+backup_file $fileToBackup  # Here we call the fuction backup_file() with its parameter
+
+if [ $? -eq 0 ]  # if the exit code = 0 means that last comand succeeded. 
 then
   echo "Backup Succeeded!!!"
+else 
+  echo "Files does not exits."
 fi
 
